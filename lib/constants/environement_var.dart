dbff5d7280f287203b09e46881a9815daa979d72
Carmdl <carmel.djegui@epitech.eu>
2025-05-29 23:58:32 +0100

    Test the stream api

--- lib/constants/environement_var.dart
+++
@@ -1,4 +0,0 @@
-const String apiUrl = "http://10.0.2.2:3001";
-const String streamApiKey = "by2qc6fxt97z";
-const String streamApiSecret =
-    "h36nsdxah6exf74gkmbqx5by5gwk53yf22g45kamw8z62ejv5bbhasnvvpur6qcv";
